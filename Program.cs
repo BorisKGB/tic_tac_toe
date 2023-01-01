7ba81bfca9ac05a8d8f3a49ff34579fa39f7191f
BorisKGB <flot3.sky@yandex.ru>
2023-01-01 23:40:49 +0300

    set main parameters, ask PlayMode from user and normalize it

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int FieldSize = 3;
+int Marks2Win = FieldSize;
+
+int PlayModeDefault = 1;
+int PlayMode;
+Console.Write(@$"Game mod can be
+1) robot vs human (default)
+2) human vs human
+3) robot vs robot
+Please input game mode number (default:{PlayModeDefault}): ");
+
+string _PlayMode = Console.ReadLine();
+if (! int.TryParse(_PlayMode, out PlayMode))
+{
+    PlayMode = 0;
+}
+
+if (PlayMode < 1 || PlayMode > 3)
+{
+    Console.WriteLine($"Incorrect input '{_PlayMode}', game mode will be set to '1'");
+    PlayMode = PlayModeDefault;
+ }
+
+Console.WriteLine($"Current game mode {PlayMode}");
